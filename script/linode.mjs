6ca8cc71207bcedc9bc107205a0dd9250e30de77
殊途 <shutu736@gmail.com>
2022-07-20 11:26:39 +0800

    debug

--- script/linode.mjs
+++ script/linode.mjs
@@ -2,11 +2,11 @@
 // import 'zx/globals'
 
 // zx setting
-$.verbose = false;
+// $.verbose = false;
 
 // 配置命令行参数
 const { username, vol, api_token, linode_id, size } = require('minimist')(process.argv.slice(2), {
-  string: ['username', 'vol', 'api_token', 'lionde_id', size],
+  string: ['username', 'vol', 'api_token', 'lionde_id', 'size'],
   default: { vol: 'vol', size: 1024 }
 })
 
