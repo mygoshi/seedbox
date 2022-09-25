9e0320c595646e05e5b870df032eb8308e6872d2
shutu <shutu736@gmail.com>
2022-09-25 19:32:22 +0800

    修改判断

--- component/seedbox_component.sh
+++ component/seedbox_component.sh
@@ -81,14 +81,12 @@ fi
 }
 
 function vnstat_update() {
-  if [ "$1" =~ "linode" ] || [ "$1" == "oracle" ]; then
-    sed -i "s/MaxBandwidth 1000/MaxBandwidth 10000/g" /etc/vnstat.conf
-    systemctl restart vnstat
+  if [[ "$1" =~ "linode" ]] || [[ "$1" == "oracle" ]]; then
+    sed -i "s/MaxBandwidth 1000/MaxBandwidth 10000/g" /etc/vnstat.conf && systemctl restart vnstat
   fi
 
-  if [ "$1" =~ "netcup" ]; then
-    sed -i "s/MaxBandwidth 1000/MaxBandwidth 3000/g" /etc/vnstat.conf
-    systemctl restart vnstat
+  if [[ "$1" =~ "netcup" ]]; then
+    sed -i "s/MaxBandwidth 1000/MaxBandwidth 3000/g" /etc/vnstat.conf && systemctl restart vnstat
   fi
 }
 
